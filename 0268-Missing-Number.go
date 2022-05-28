f0a21edca33a89e766837abc01dfe3b771d94c84
mikezh <liwenzheng.mike@gmail.com>
2022-05-28 22:32:10 +0800

    0268

---
+++ 0268-Missing-Number.go
@@ -0,0 +1,15 @@
+
+
+func missingNumber(nums []int) int {
+	var sumIndex = 0
+	var sumValue = 0
+	for i, v := range nums {
+		sumIndex += i
+		sumValue += v
+
+	}
+
+	sumIndex += len(nums)
+
+	return sumIndex - sumValue
+}
