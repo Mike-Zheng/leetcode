37dc645be71ce63a67eb126677b252b4e00bd7f4
mikezh <liwenzheng.mike@gmail.com>
2022-06-12 00:12:31 +0800

    1658

--- 1658-Minimum-Operations-to-Reduce-X-to-Zero.js
+++ 1658-Minimum-Operations-to-Reduce-X-to-Zero.js
@@ -3,7 +3,7 @@
  * @param {number} x
  * @return {number}
  */
- var minOperations = function (nums, x) {
+var minOperations = function (nums, x) {
     let length = nums.length;
     let target = nums.reduce((acc, i) => acc + i, 0) - x;
 
@@ -12,9 +12,14 @@
     var ans = -1;
     for (let r = 0; r < length; r++) {
         sum += nums[r];
-        while (l <= r && sum > target)
-            sum -= nums[l++];
-        if (target == sum) ans = Math.max(ans, r - l + 1);
+        while (l <= r && sum > target) {
+            l++;
+            sum -= nums[l];
+        }
+
+        if (target == sum) {
+            ans = Math.max(ans, r - l + 1);
+        }
     }
 
     return ans == -1 ? -1 : length - ans;
