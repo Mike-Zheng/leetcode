83f60897ec3c7c57397ea922cfe3bc3d8adbc3d2
mikezh <liwenzheng.mike@gmail.com>
2022-06-12 00:10:57 +0800

    1658

--- 1658-Minimum-Operations-to-Reduce-X-to-Zero.js
+++ 1658-Minimum-Operations-to-Reduce-X-to-Zero.js
@@ -3,44 +3,19 @@
  * @param {number} x
  * @return {number}
  */
-var minOperations = function (nums, x) {
+ var minOperations = function (nums, x) {
     let length = nums.length;
+    let target = nums.reduce((acc, i) => acc + i, 0) - x;
 
-    let sum = nums.reduce((acc, i) => acc + i, 0);
-    let output = -1;
-
-    let preSumHash = {};
-
-    preSumHash[[0, 0]] = nums[0];
-    for (let j = 1; j < length; j++) {
-        preSumHash[[0, j]] = preSumHash[[0, j - 1]] + nums[j];
+    var sum = 0;
+    var l = 0;
+    var ans = -1;
+    for (let r = 0; r < length; r++) {
+        sum += nums[r];
+        while (l <= r && sum > target)
+            sum -= nums[l++];
+        if (target == sum) ans = Math.max(ans, r - l + 1);
     }
 
-    for (let i = 1; i < length; i++) {
-        for (let j = i; j < length; j++) {
-            preSumHash[[i, j]] = preSumHash[[i - 1, j]] - preSumHash[[i - 1, i - 1]];
-        }
-    }
-
-    if (sum == x) {
-        output = length;
-    }
-
-    for (let i = 0; i < length; i++) {
-        for (let j = length - 1; j >= i; j--) {
-            let scopeSum = preSumHash[[i, j]];
-
-            if (scopeSum == sum - x) {
-                let ans = length - j + i - 1;
-                output = output > -1 ? Math.min(output, ans) : ans;
-                break;
-            }
-
-            if (scopeSum < sum - x) {
-                break;
-            }
-        }
-    }
-
-    return output;
+    return ans == -1 ? -1 : length - ans;
 };
