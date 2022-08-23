f280bcc72cc066828b8406a975495eef67d59cf1
mikezheng <liwenzheng.mike@gmail.com>
2022-08-23 10:06:02 +0800

    0342

--- 0342-Power-of-Four.js
+++ 0342-Power-of-Four.js
@@ -2,8 +2,6 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-  return n === 1 ? true : /^100(00)*$/.test(n.toString(2));
+ var isPowerOfFour = function (n) {
+  return /^1(0)*$/.test(n.toString(4));
 };
-
-
